d94681ab146a172d2e7bd887ff3199ab685feac7
Adam0091 <55671230+Adam0091@users.noreply.github.com>
2021-12-03 20:27:57 +0400

    Add: React project(MapGL)

---
+++ React_map_gl/precache-manifest.82f0fb76c884db1b6429c4722fc32c2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c016e3f92420e19ecd607d4ccdb28c",
+    "url": "/test_react_map_gl/index.html"
+  },
+  {
+    "revision": "c1bcd1e5fb37c1880150",
+    "url": "/test_react_map_gl/static/css/2.b0618883.chunk.css"
+  },
+  {
+    "revision": "89753842a377df41092d",
+    "url": "/test_react_map_gl/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c1bcd1e5fb37c1880150",
+    "url": "/test_react_map_gl/static/js/2.91c29991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_react_map_gl/static/js/2.91c29991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89753842a377df41092d",
+    "url": "/test_react_map_gl/static/js/main.2cf735ae.chunk.js"
+  },
+  {
+    "revision": "979f3e1fff29e8d94f98",
+    "url": "/test_react_map_gl/static/js/runtime-main.fb47be99.js"
+  }
+]);
